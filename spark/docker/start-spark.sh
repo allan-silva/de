48d305a930d13f13ecac0832e820caacd816daf3
Allan Silva <allan@allansilva.com.br>
2023-03-30 05:56:39 -0300

    Dockerize Spark for learn purposes

--- spark/docker/start-spark.sh
+++ spark/docker/start-spark.sh
@@ -3,10 +3,24 @@
 if test "$1" = "main-node"
 then 
     echo "Starting main node"
-    start-master.sh
+    $SPARK_HOME/sbin/start-master.sh
     supervisord -c supervisor.conf
-else
+elif test "$1" = "worker-node"
+then
     echo "Start worker node - Main host: $MAIN_HOST"
-    start-worker.sh spark://$MAIN_HOST:7077
+    $SPARK_HOME/sbin/start-worker.sh spark://$MAIN_HOST:7077
     supervisord -c supervisor.conf
+elif test "$1" = "spark-submit"
+then
+    echo "Runnig command: spark-submit --master spark://$MAIN_HOST:7077 ${@:2}"
+    $SPARK_HOME/bin/spark-submit ${@:2}
+elif test "$1" = "history-server"
+then
+    $SPARK_HOME/sbin/start-history-server.sh
+    supervisord -c supervisor.conf
+elif test "$1" = "shell"
+then
+    /bin/bash
+else
+    echo "Wrong parameters $1"
 fi
